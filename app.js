5f406d9d55d2f486aad0432197b2ed56072ff545
xiaokaixuan <kaixuan1115@126.com>
2018-08-22 19:07:33 +0800

    Init commit

---
+++ app.js
@@ -0,0 +1,301 @@
+﻿'use strict';
+
+var mod_url = require('url'), https = require('https');
+var _ = require('lodash');
+var log4js = require('log4js'), logger = log4js.getLogger();
+var cheerio = require('cheerio'), FormData = require('form-data');
+
+var firstTabCreated = false;                  // was the tab created at least once?
+var createdTabId;                             // tabid created for the plugin
+var url;                                      // 11 digits sent from the origin
+var length = 0;                               // number of secs to watch the video
+var interval;
+var DELAY = 0.1;
+var isRunning = true;                         // is the pluggin runnin?
+var servCount = 0;                            // amount of videos requested
+var credCount = 0;                            // credits earnt for videos watched
+var credits = parseFloat(0);                  // amount of credits worth for the video requested
+var currentVideoCredits = parseFloat(0);      // amount of credits of current video
+var autostart = false;                        // will the plugin start on launch?
+var secid = 0;                                // secid comes when the user logs in
+var logged_userid;                            // userid of the user logged
+var logged_passwd;                            // passwd of the user logged
+var logged_secid;                             // secid of the user logged
+var version = '2.4.3';                        // chrome.runtime.getManifest().version;
+var adjust = 0;                               // Es el tag que usa para eliminar creditos por algun motivo
+var adjustmsg;                                // Es el motivo por el cual los creditos fueron eliminados
+var ads;                                      // Pide el codigo fuente de la pagina
+var youTubeSourceCode;
+var connectivitySpeedTabId = 0;
+var connectivitySpeed = 0;
+var model = 'Firefox';
+var sid;                                      // sid comes in the request
+var wsubs;                                    // 0 - auto sub disabled, 1 - auto sub enabled
+var wlikes;                                   // 0 - auto like disabled, 1 - auto like enabled
+var wcomms;                                   // auto comment
+var videoWatchedCounter = 0;
+var time;
+var youTubeDetailsTabId;
+var youTubeUserId = "";
+var youTubeChannelId = "";
+var youTubeVideoDuration = 0;
+var youTubeCid;
+var subed = false;
+var log = [{
+  url: "No request has been made yet.",
+  error: "No errors detected. Everything is working as expected.",
+  time: " ",
+  currentVideoCredits: 0
+}];                                           // Object to log important data
+
+/*
+ * Si la velocidad es 0, necesito obtenerla. Tambien la informacion de YouTube.
+ * Lo desactivo porque si verifico al inicio, FireFox no lo aprueba por abrir la ventana.
+if(connectivitySpeed == 0) {
+  openConnectivitySpeedTab();
+}
+
+if(youTubeUserId == "") {
+  openYouTubeDetailsTab();
+}
+*/
+
+/*
+ * Https POST Request
+*/
+var httpsPost = function (urlString, formData, callback) {
+  var options = mod_url.parse(urlString);
+  options.method = 'POST';
+  options.headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:61.0) Gecko/20100101 Firefox/61.0"
+  };
+  options.headers['Content-Type'] = formData.getHeaders()['content-type'];
+  options.headers['Content-Length'] = formData.getLengthSync();
+  var req = https.request(options, res => {
+    res.setEncoding('utf8');
+    var data = '';
+    res.on('data', d => data += d);
+    res.on('end', () => callback(null, data));
+  });
+  req.on('error', err => callback(err));
+  formData.pipe(req);
+};
+
+/*
+ * Https GET Request
+*/
+var httpsGet = function (urlString, callback) {
+  var id = httpsGet.tid || 1;
+  httpsGet.tid = id + 1;
+  var options = mod_url.parse(urlString);
+  options.headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:61.0) Gecko/20100101 Firefox/61.0"
+  };
+  return https.get(urlString, (res) => {
+    res.on('data', () => void 0);
+    res.on('end', () => callback({ id }));
+  }).on('error', () => callback({ id }));
+};
+
+/*
+ * Base64
+*/
+var btoa = str => new Buffer(str).toString('base64');
+
+/*
+ * Funcion principal. Es donde hago los pedidos de videos
+*/
+function sendData() {
+  try {
+    var dataToSend = "userid=" + logged_userid
+      + "&version=" + version
+      + "&videoid=" + url
+      + "&model=" + model
+      + "&sid=" + sid
+      + "&secid=" + logged_secid
+      + "&speed=" + connectivitySpeed
+      + "&youtubeUserId=" + youTubeUserId
+      + "&youtubeChannelId=" + youTubeChannelId
+      + "&youTubeVideoDuration=" + youTubeVideoDuration
+      + "&cid=" + youTubeCid
+      + "&subed=" + subed;
+
+    var encryptedData = btoa(dataToSend);
+    var FD = new FormData();
+    FD.append('userid', logged_userid);
+    FD.append('data', encryptedData);
+
+    var completeFun = function (error, data) {
+      if (error) throw error;
+      var $ = cheerio.load(data);
+      url = $('url').text();
+      length = parseFloat($('length').text());
+      credits = parseFloat($('credits').text());
+      currentVideoCredits = length / 30;
+      sid = $('sid').text();
+      wsubs = $('wsubs').text();
+      wlikes = $('wlikes').text();
+      adjust = $('adjust').text();
+      adjustmsg = $('adjustmsg').text();
+      ads = $('ads').text();
+      var error = $('error').text();
+      if (error) {
+        error = "Error detected: " + error;
+      } else {
+        error = "Everything is working as expected.";
+      }
+      var tempTime = new Date().getHours() + ":";
+      var tempMinutes = + new Date().getMinutes();
+      if (parseFloat(tempMinutes) < 10) {
+        time = tempTime + "0" + tempMinutes;
+      } else {
+        time = tempTime + tempMinutes;
+      }
+      logger.info('Tip:', error);
+      logger.info('Video:', url, 'currentVideoCredits:', currentVideoCredits, 'creditsAdjust', adjustmsg);
+
+      // Every 20 video requests I try to update the connectivity speed again.
+      videoWatchedCounter++;
+      if (videoWatchedCounter == 1 || videoWatchedCounter % 20 == 0) {
+        openConnectivitySpeedTab();
+      }
+      if (videoWatchedCounter == 1) {
+        openYouTubeDetailsTab();
+      }
+
+      // Verifico que la tab exista para saber si debo crear una nueva o actualizar una existente
+      checkIfTabExistsCallback(url, length);
+      // Al crear o actualizar, se incrementa la cantidad de pedidos exitosos
+      servCount++;
+      // Tambien debo sumar los creditos obtenidos
+      if (credits != 0) { // caso contrario anida 0 a la izquierda
+        credCount += parseFloat(credits);
+      }
+      // Elimino creditos segun sea necesario
+      credCount -= parseFloat(adjust);
+      if (ads == 1) {
+        sendSourceCode();
+      }
+      logger.info('credCount:', credCount);
+      // Las alarmas se encargan de hacer nuevos pedidos
+      createAlarm(length);
+    }
+    httpsPost('https://vagex.com/fupdater2.php', FD, completeFun);
+  } catch (e) {
+    logger.error('sendData Exception:', e);
+  }
+}
+
+/*
+ * Funcion de login para obtener el secid, necesario para pedir videos para el usuario
+ * junto al userid y su passwd
+*/
+function doLogin(userid, passwd, sendResponse) {
+  try {
+    var FD = new FormData();
+    FD.append('userid', userid);
+    FD.append('passwd', passwd);
+
+    var completeFun = function (error, data) {
+      if (error) throw error;
+      var $ = cheerio.load(data);
+      secid = $('secid').text();
+      if (!secid) {
+        logger.warn('Login faild!', data);
+        return;
+      }
+      logged_userid = userid;
+      logged_passwd = passwd;
+      logged_secid = secid;
+      logger.info('Login success!');
+      process.nextTick(sendData);
+    }
+    httpsPost('https://vagex.com/alogin.php', FD, completeFun);
+  } catch (e) {
+    logger.error('doLogin Exception:', e);
+  }
+} doLogin('437470', 'xxxxxx');
+
+/*
+ * Verifico si existe la pestaña para saber si debo actualizar una existente
+ * para no abrir multiples pestañas, o bien crear una nueva
+*/
+function checkIfTabExistsCallback(url, length) {
+  if (!firstTabCreated) {
+    openNewBackgroundTab(url, length);
+  } else {
+    updateBackgroundTab(url, length);
+  }
+}
+
+/*
+ * Abro una pestaña nueva cuando no existe una del plugin
+*/
+function openNewBackgroundTab(url, length) {
+  httpsGet('https://www.youtube.com/watch?v=' + url, function (tab) {
+    firstTabCreated = true;
+    createdTabId = tab.id;
+    logger.info('openNewBackgroundTab length:', length, 'tabId:', createdTabId);
+  });
+}
+
+/*
+ * Actualizo una pestaña existente para no abrir multiples pestañas
+*/
+function updateBackgroundTab(url, length) {
+  openNewBackgroundTab(url, length);
+}
+
+/*
+ * Abro una pestaña para conocer la velocidad de la conexion
+*/
+function openConnectivitySpeedTab() {
+  var downloadSize = 206037, urlString = 'https://vagex.com/myimage.jpg?n=' + Math.random();
+  var startTime = Date.now();
+  httpsGet(urlString, function (tab) {
+    var endTime = Date.now();
+    var duration = Math.round((endTime - startTime) / 1000);
+    if (duration == 0) duration = 1;
+    var bitsLoaded = downloadSize * 8;
+    var speedBps = Math.round(bitsLoaded / duration);
+    var speedKbps = (speedBps / 1024).toFixed(2);
+    connectivitySpeed = speedKbps;
+    connectivitySpeedTabId = tab.id;
+    logger.info('openConnectivitySpeedTab:', speedKbps, 'tabId:', tab.id);
+  });
+}
+
+/*
+ * Abro una pestaña para obtener el usuario de YouTube y el canal
+*/
+function openYouTubeDetailsTab() {
+  httpsGet('https://www.youtube.com/account_advanced', function (tab) {
+    youTubeDetailsTabId = tab.id;
+    logger.info('openYouTubeDetailsTab tabId:', youTubeDetailsTabId);
+  });
+}
+
+/*
+ * Crea una alarma para pedir el proximo video. La alarma sera creada en base
+ * a la cantidad de segundos que se necesita ver el video, mas un numero random
+ * entre 5 y 10 segundos (por pedido)
+*/
+function createAlarm(length) {
+  var random = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+  length = parseFloat(length) + parseFloat(random);
+  var timer = setTimeout(() => {
+    clearTimeout(timer); sendData();
+  }, length * 1000);
+}
+
+function sendSourceCode() {
+  try { // Envio el sourcecode
+    var FD = new FormData();
+    FD.append('userid', logged_userid);
+    FD.append('ads', youTubeSourceCode);
+    httpsPost('https://vagex.com/ffads.php', FD, () => void 0);
+  } catch (e) {
+    logger.error('sendSourceCode Exception!', e);
+  }
+}
+
